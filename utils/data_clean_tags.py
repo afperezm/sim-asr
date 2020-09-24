82b6290bf17602e72794de11799bd11b29258002
Andres Perez <andres.perez@comisiondelaverdad.co>
2020-09-24 02:39:06 -0500

    Extend expressions to clean hours that should not be verbalized

--- utils/data_clean_tags.py
+++ utils/data_clean_tags.py
@@ -780,9 +780,9 @@ def main():
                           r":?\s\d{2}:\d{2}\s-\s\d{2}:\d{2}\)",
                           "", contents, flags=re.IGNORECASE)
         contents = re.sub(r"\(Datos\ssensibles(:\s\d{2}:\d{2})?\)", "", contents)
-        contents = re.sub(r"\d:\d{2}:\d{2}", "", contents)
-        contents = re.sub(r"1[3-9]:\d{2}", "", contents)
-        contents = re.sub(r"[2-9]\d:\d{2}", "", contents)
+        contents = re.sub(r"\d{1,2}:\d{2}:\s?\d{2}", "", contents)
+        contents = re.sub(r"1[3-9]:\d{2}\s?-?\s?", "", contents)
+        contents = re.sub(r"[2-9]\d:\s?\d{2}\s?-?\s?", "", contents)
         contents = re.sub(r"PAUSA(\d{2}:\d{2})?", "", contents)
 
         standard_actor_tags = [
