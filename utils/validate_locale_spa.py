308925adce0f82d1cec8b8a75c9d962ae06702d2
Andres Perez <andres.perez@comisiondelaverdad.co>
2020-10-13 12:00:05 -0500

    Create function to validate and normalize spanish transcription labels

---
+++ utils/validate_locale_spa.py
@@ -0,0 +1,25 @@
+import re
+
+
+def validate_label(label):
+    """ Validate and normalize transcriptions. Returns a cleaned version of the label or None if it's invalid.  """
+
+    if re.search(r"[0-9]|[()<>\[\]&*{}]", label) is not None:
+        return None
+
+    label = label.replace("-", " ")
+    label = label.replace("_", " ")
+    label = re.sub("[ ]{2,}", " ", label)
+    label = label.replace(".", "")
+    label = label.replace(",", "")
+    label = label.replace(":", "")
+    label = label.replace(";", "")
+    label = label.replace("¿", "")
+    label = label.replace("?", "")
+    label = label.replace("¡", "")
+    label = label.replace("!", "")
+    label = label.replace("\"", "")
+    label = label.strip()
+    label = label.lower()
+
+    return label if label else None
