eaa76249325f8e033d4cd39e226a6ca8876c030f
Andres Perez <andres.perez@comisiondelaverdad.co>
2020-11-26 16:41:48 -0500

    Replace known punctation signs and symbols to allow label validation

--- utils/validate_locale_spa.py
+++ utils/validate_locale_spa.py
@@ -3,11 +3,12 @@ import re
 
 def validate_label(label):
     """ Validate and normalize transcriptions. Returns a cleaned version of the label or None if it's invalid.  """
-
-    if re.search(r"[0-9]|[()<>\[\]&*{}]", label) is not None:
+    if re.search(r"[0-9]", label) is not None:
         return None
 
     label = label.replace("-", " ")
+    label = label.replace("–", " ")
+    label = label.replace("—", " ")
     label = label.replace("_", " ")
     label = re.sub("[ ]{2,}", " ", label)
     label = label.replace(".", "")
@@ -19,6 +20,34 @@ def validate_label(label):
     label = label.replace("¡", "")
     label = label.replace("!", "")
     label = label.replace("\"", "")
+    label = label.replace("\'", "")
+    label = label.replace("`", "")
+    label = label.replace("´", "")
+    label = label.replace("’", "")
+    label = label.replace("“", "")
+    label = label.replace("”", "")
+    label = label.replace("(", "")
+    label = label.replace(")", "")
+    label = label.replace("<", "")
+    label = label.replace(">", "")
+    label = label.replace("[", "")
+    label = label.replace("]", "")
+    label = label.replace("{", "")
+    label = label.replace("}", "")
+    label = label.replace("&", "")
+    label = label.replace("*", "")
+    label = label.replace("%", "")
+    label = label.replace("$", "")
+    label = label.replace("@", "")
+    label = label.replace("=", "")
+    label = label.replace("#", "")
+    label = label.replace("+", "")
+    label = label.replace("^", "")
+    label = label.replace("\\", "")
+    label = label.replace("/", "")
+    label = label.replace("|", "")
+    label = label.replace("\t", "")
+    label = label.replace("\x7f", "")
     label = label.strip()
     label = label.lower()
 
