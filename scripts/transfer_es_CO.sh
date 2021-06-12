3d7721e5755fdee627d6573337e22485062797da
Andres Perez <andres.perez@comisiondelaverdad.co>
2021-06-12 03:56:32 -0500

    Increase batch size and number of epochs

--- scripts/transfer_es_CO.sh
+++ scripts/transfer_es_CO.sh
@@ -8,9 +8,9 @@ summ_dir="/home/andresf/summaries"
 python -u DeepSpeech.py --noshow_progressbar \
        --alphabet_config_path "${mdls_dir}/cclmtv_es/alphabet.txt" \
        --train_files "${data_dir}/output_train.csv" \
-       --train_batch_size 32 \
+       --train_batch_size 64 \
        --dev_files "${data_dir}/output_dev.csv" \
-       --dev_batch_size 32 \
+       --dev_batch_size 64 \
        --save_checkpoint_dir "${ckpt_dir}/ds-transfer-es_CO/" \
        --load_checkpoint_dir "${ckpt_dir}/cclmtv_es/" \
        --summary_dir "${summ_dir}/ds-transfer-es_CO/" \
@@ -18,7 +18,7 @@ python -u DeepSpeech.py --noshow_progressbar \
        --dropout_rate 0.25 \
        --learning_rate 0.0001 \
        --n_hidden 2048 \
-       --epochs 100 \
+       --epochs 500 \
        --early_stop True \
        --es_epochs 25 \
        --reduce_lr_on_plateau True \
