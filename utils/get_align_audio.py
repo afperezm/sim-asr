1324961e7a7133f4c99116db427927329999b99e
Maria Ortiz <maria.ortiz@comisiondelaverdad.co>
2021-07-12 11:35:23 -0500

    Reading more than one str file and getting total time

--- utils/get_align_audio.py
+++ utils/get_align_audio.py
@@ -1,28 +1,40 @@
 import pysrt
+import glob, os
 from datetime import datetime, date, timedelta
 
-# ----- Functions ------- #
-def to_td(h):
-    ho, mi, se = h.split(':')
-    return timedelta(hours=int(ho), minutes=int(mi), seconds=int(se))
-
 # ------ Main ------- #
-subs = pysrt.open('../../audios/001-VI-00003.srt')
-lines = len(subs)
-times = []
 
-for i in range(lines):
-    start = subs[i].start.to_time()
-    end = subs[i].end.to_time()
-    print("Start", start)
-    print("End", end)
-    diff = datetime.combine(date.min, end) - datetime.combine(date.min, start) 
-    times.append(diff)
-    print(diff)
-    print("-----")
+total_times = []
+dir = "../../audios/"
+
+for file in os.listdir(dir):
+    if file.endswith(".srt"):
+        subs = pysrt.open(os.path.join(dir, file))
+        lines = len(subs)
+        times_per_line = []
+        #print("Subs for", file, ":", lines)
+    
+        for i in range(lines):
+            start = subs[i].start.to_time()
+            end = subs[i].end.to_time()
+            #print("Start", start)
+            #print("End", end)
+            diff = datetime.combine(date.min, end) - datetime.combine(date.min, start) 
+            times_per_line.append(diff)
+            #print(diff)
+            #print("-----")
+
+        total_per_file = timedelta(0)
+        for i in range(len(times_per_line)):
+            total_per_file  = total_per_file + times_per_line[i]
+
+        print("Total time for ", file,str(total_per_file))
+    total_times.append(total_per_file)
 
 total = timedelta(0)
-for i in range(len(times)):
-    total = total + times[i]
+for i in range(len(total_times)):
+    total  = total + total_times[i]
 
-print("Total", total)
+print("Total time for all", str(total))
+
+
