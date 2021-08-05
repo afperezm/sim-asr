e69d0f6405f177194ba3d9a973d4a126de11c21c
Maria Ortiz <maria.orti@comisiondelaverdad.co>
2021-08-05 13:07:34 -0500

    Adding script to transcribe multiple audios with different checkpoints

---
+++ utils/mass-transcription.sh
@@ -0,0 +1,29 @@
+source ~/ds-train-venv/bin/activate
+
+echo "----PROCESS BEGINGS---"
+date
+days="$(date +'%Y%m%d-%H%M')"
+result_path=$HOME/audios_test_result
+models=(cclmtv_es ds-transfer-es_CO_exp1 ds-transfer-es_CO_exp2 ds-transfer-es_CO_exp3 ds-transfer-es_CO_exp4 ds-transfer-es_CO_exp5)
+
+for x in wav; do
+	for i in ~/audios_test/*."${x}"; do
+		filename=$(basename "${i%.*}")
+   		echo "----------Working on "${filename}" file---------"
+		mkdir $result_path/$filename
+		for model in ${models[@]}; do
+		trans_filename=${result_path}/${filename}/${filename}-${model}.json
+		echo ${trans_filename}
+		if test -f ${trans_filename};
+			then
+				echo "Wav already processed"
+			else
+				echo "Transcription for "${filename}" and model "${model}" starts at "${days}""
+				python3 ~/workspace/DeepSpeech/transcribe.py --src ${i} --dst ${result_path}/${filename}/${filename}-${model}.json --force --alphabet_config_path $HOME/models/cclmtv_es/alphabet.txt --load_checkpoint_dir $HOME/checkpoints/${model}/ --load_evaluate "best" --scorer_path $HOME/models/cclmtv_es/kenlm_es.scorer  --n_hidden 2048 --load_cudnn
+				echo "Transcription for "${filename}" finished at "${days}""
+		fi
+		done	
+	done
+done
+
+
